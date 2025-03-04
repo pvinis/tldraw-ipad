f4fd3bd1c9d725865109cbec94f03fe96af8d453
Pavlos Vinieratos <pvinis@gmail.com>
2025-03-04 23:50:01 +0200

    magic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,8 @@ import { WebView } from "react-native-webview";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-red-500 py-safe">
-      <WebView className="flex-1" source={{ uri: "https://google.com" }} />
+    <View className="flex-1 bg-red-500  pt-safe">
+      <WebView className="flex-1" source={{ uri: "https://tldraw.com" }} />
     </View>
   );
 }
