4bdc425f53e9a573ad8ed37392fe3b2c269f86a5
Pavlos Vinieratos <pvinis@gmail.com>
2025-03-05 01:07:43 +0200

    no scrolling of the webview

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,11 @@ import { WebView } from "react-native-webview"
 export default function Index() {
   return (
     <View className="pt-safe flex-1 bg-black">
-      <WebView className="flex-1" source={{ uri: "https://tldraw.com" }} />
+      <WebView
+        className="flex-1"
+        scrollEnabled={false}
+        source={{ uri: "https://tldraw.com" }}
+      />
     </View>
   )
 }
