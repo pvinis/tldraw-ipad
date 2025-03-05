3c1699fe9a4ede74b312b0194cfa99325d93036d
Pavlos Vinieratos <pvinis@gmail.com>
2025-03-05 11:16:44 +0200

    aha

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,18 @@
 import { View } from "react-native"
 import { WebView } from "react-native-webview"
+import { cssInterop } from "nativewind"
+
+cssInterop(WebView, { className: "style" })
 
 export default function Index() {
   return (
     <View className="pt-safe flex-1 bg-black">
       <WebView
-        className="flex-1"
-        scrollEnabled={false}
+        // className="my-2 flex-1 bg-red-300"
+        className="my-2 bg-red-300 py-2"
+        // scrollEnabled={false}
+        // nestedScrollEnabled={false}
+        // overScrollMode="never"
         source={{ uri: "https://tldraw.com" }}
       />
     </View>
